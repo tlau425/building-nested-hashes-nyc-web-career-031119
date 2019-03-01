6e4e63c65e53405a5ebbd6a688facdf961ca89c5
Tommy Lau <tlau425@yahoo.com>
2019-03-01 02:24:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {:name => "Benvolio", :age => "17", :attitude => "worried"}
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -15,6 +16,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "Steven", :age => "44", :attitude => "worried"}
       ]
    }
 }
