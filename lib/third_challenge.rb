95a40d875cdc66f4cb3a9bdf308f2357d400e204
Tommy Lau <tlau425@yahoo.com>
2019-03-01 02:19:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         :name => "Romeo",
-        :age => 15,
+        :age => "15",
         :status => "Alive"
         },
       :hero_friends => []
